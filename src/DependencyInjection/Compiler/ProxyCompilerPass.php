1d8c5aedf2863d07c85ac7770fb479a6706d0434
vcorre <vcorre@pielectronique.com>
2019-11-28 15:10:01 +0100

    Factorise configuration function

--- src/DependencyInjection/Compiler/ProxyCompilerPass.php
+++ src/DependencyInjection/Compiler/ProxyCompilerPass.php
@@ -11,6 +11,7 @@ namespace Pyrowman\PheanstalkBundle\DependencyInjection\Compiler;
 use Pyrowman\PheanstalkBundle\Exceptions\PheanstalkException;
 use Pheanstalk\Pheanstalk;
 use Pheanstalk\PheanstalkInterface;
+use Pyrowman\PheanstalkBundle\PheanstalkLocator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
@@ -22,6 +23,8 @@ use Symfony\Component\DependencyInjection\Definition;
  */
 class ProxyCompilerPass implements CompilerPassInterface
 {
+    protected $defaultPheanstalkName = null;
+
     protected function reservedName()
     {
         return [
@@ -35,62 +38,85 @@ class ProxyCompilerPass implements CompilerPassInterface
     }
 
     /**
-     * @inheritdoc
+     * @param ContainerBuilder $container
+     *
+     * @throws PheanstalkException
      */
     public function process(ContainerBuilder $container)
     {
-        $defaultPheanstalkName = null;
-        $pheanstalks           = $container->getParameter('pyrowman.pheanstalk.pheanstalks');
-
-        $pheanstalkLocatorDef = $container->getDefinition('pyrowman.pheanstalk.pheanstalk_locator');
+        $pheanstalks = $container->getParameter('pyrowman.pheanstalk.pheanstalks');
 
         // For each connection in the configuration file
         foreach ($pheanstalks as $name => $pheanstalk) {
-            if (in_array($name, $this->reservedName())) {
-                throw new \RuntimeException('Reserved pheanstalk name: '.$name);
-            }
+            $this->addServer($name, $pheanstalk, $container);
+        }
+    }
 
-            $pheanstalkConfig = [
-                $pheanstalk['server'],
-                $pheanstalk['user'] ?? null,
-                $pheanstalk['password'] ?? null,
-                $pheanstalk['port'],
-                $pheanstalk['timeout']
-            ];
-            $isDefault = $pheanstalk['default'];
+    /**
+     * @param string           $name
+     * @param array            $pheanstalk
+     * @param ContainerBuilder $container
+     *
+     * @throws PheanstalkException
+     */
+    protected function addServer($name, array $pheanstalk, ContainerBuilder $container)
+    {
+        $pheanstalkLocatorDef = $container->getDefinition('pyrowman.pheanstalk.pheanstalk_locator');
+        if (in_array($name, $this->reservedName())) {
+            throw new \RuntimeException('Reserved pheanstalk name: ' . $name);
+        }
 
-            # @see https://github.com/armetiz/pyrowmanPheanstalkBundle/issues/61
-            $pheanstalkDef = clone $container->getDefinition($pheanstalk['proxy']);
+        $pheanstalkConfig = [
+            $pheanstalk['server'],
+            $pheanstalk['user'] ?? null,
+            $pheanstalk['password'] ?? null,
+            $pheanstalk['port'],
+            $pheanstalk['timeout']
+        ];
+        $isDefault = $pheanstalk['default'];
 
-            $pheanstalkDef->addMethodCall('setPheanstalk', [new Definition(Pheanstalk::class, $pheanstalkConfig)]);
-            $pheanstalkDef->addMethodCall('setName', [$name]);
-            $pheanstalkDef->setPublic(true);
+        # @see https://github.com/armetiz/pyrowmanPheanstalkBundle/issues/61
+        $pheanstalkDef = clone $container->getDefinition($pheanstalk['proxy']);
 
-            $container->setDefinition('pyrowman.pheanstalk.'.$name, $pheanstalkDef);
+        $pheanstalkDef->addMethodCall('setPheanstalk', [new Definition(Pheanstalk::class, $pheanstalkConfig)]);
+        $pheanstalkDef->addMethodCall('setName', [$name]);
+        $pheanstalkDef->setPublic(true);
 
-            // Register the connection in the connection locator
-            $pheanstalkLocatorDef->addMethodCall('addPheanstalk', [
-                $name,
-                $container->getDefinition('pyrowman.pheanstalk.'.$name),
-                $isDefault,
-            ]);
+        $container->setDefinition('pyrowman.pheanstalk.' . $name, $pheanstalkDef);
 
-            if ($isDefault) {
-                if (null !== $defaultPheanstalkName) {
-                    throw new PheanstalkException(sprintf('Default pheanstalk already defined. "%s" & "%s"', $defaultPheanstalkName, $name));
-                }
+        // Register the connection in the connection locator
+        $pheanstalkLocatorDef->addMethodCall('addPheanstalk', [
+            $name,
+            $container->getDefinition('pyrowman.pheanstalk.' . $name),
+            $isDefault,
+        ]);
 
-                $defaultPheanstalkName = $name;
-                $legacyAlias = $container->setAlias('pyrowman.pheanstalk', 'pyrowman.pheanstalk.'.$name);
-                if ($legacyAlias) {
-                    $legacyAlias->setPublic(true);
-                }
+        if ($isDefault) {
+            $this->autowireDefaultConfig($name, $container);
+        }
+    }
 
-                $autoWiringAlias = $container->setAlias(PheanstalkInterface::class, 'pyrowman.pheanstalk');
-                if ($autoWiringAlias) {
-                    $autoWiringAlias->setPublic(true);
-                }
-            }
+    /**
+     * @param string $name
+     * @param ContainerBuilder $container
+     *
+     * @throws PheanstalkException
+     */
+    protected function autowireDefaultConfig($name, ContainerBuilder $container)
+    {
+        if (null !== $this->defaultPheanstalkName) {
+            throw new PheanstalkException(sprintf('Default pheanstalk already defined. "%s" & "%s"', $this->defaultPheanstalkName, $name));
+        }
+
+        $this->defaultPheanstalkName = $name;
+        $legacyAlias = $container->setAlias('pyrowman.pheanstalk', 'pyrowman.pheanstalk.' . $name);
+        if ($legacyAlias) {
+            $legacyAlias->setPublic(true);
+        }
+
+        $autoWiringAlias = $container->setAlias(PheanstalkInterface::class, 'pyrowman.pheanstalk');
+        if ($autoWiringAlias) {
+            $autoWiringAlias->setPublic(true);
         }
     }
 }
